747cf1580c6a0af8da96bcf1b5af8c60c1f21729
maxnaro <147180357+maxnaro@users.noreply.github.com>
2023-10-17 13:05:53 +0100

    Started roughing-out manager dashboard

---
+++ pages/manager_dashboard.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ></script>    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    
+</body>
