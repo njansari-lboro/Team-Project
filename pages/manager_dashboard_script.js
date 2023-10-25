99b3038f808641e2402429f3e3f0203944a6e0c7
maxnaro <147180357+maxnaro@users.noreply.github.com>
2023-10-25 14:30:00 +0100

    Seperated style and script files - added global.css support and dark mode works with some ugly css

---
+++ pages/manager_dashboard_script.js
@@ -0,0 +1,105 @@
+$(document).ready(() => {            
+    let projectDropdown = $("#project_dropdown").get(0);
+    let projectNames = ["Project 1", "Project 2"];
+    projectNames.forEach((projectName) =>{
+        let option = document.createElement("option");
+        option.text = projectName
+        projectDropdown.add(option);
+    });
+
+    // Example data
+    const projectData = {
+        project1: {
+            data: [64, 18, 12],
+            overdue: [
+                ["John Cena", "UI Design", "4"],
+                ["Jawn Seyna", "API Integration", "41"],
+                ["Sion Cena", "Database Design and Optimisation", "1"]
+            ],
+            imminent: [
+                ["Jean Sienna", "Unit Testing", "19/10/2023"],
+                ["Zhong Xina", "Bug Fixing", "31/10/2023"],
+                ["Giannis Sina", "Get Manager a Coffee", "19/11/2023"]
+            ],
+            deadline: "06/11/2023",
+        },
+        project2: {
+            data: [10, 96, 7],
+            overdue: [
+                ["Yohan Zena", "Cross-Platform Optimisation", "6"],
+                ["Janos Szena", "Documentation", "91"],
+                ["Jan Sina", "Security Assessment", "11"]
+            ],
+            imminent: [
+                ["Juan Senna", "Performance Optimisation", "19/10/2023"],
+                ["Zhong Xina", "Code Refactoring", "23/10/2023"],
+                ["Jone Sainah", "Code Review", "01/11/2023"]
+            ],
+            deadline: "23/12/2023",
+        },
+    };
+
+    let ctx = $("#progress_chart").get(0).getContext("2d");        
+
+    let defaultProject = "project1";
+    let data = projectData[defaultProject].data;
+    let overdue = projectData[defaultProject].overdue;
+    let imminent = projectData[defaultProject].imminent; 
+    let deadline = projectData[defaultProject].deadline;
+
+    $("#date_picker").datepicker({ 
+        minDate: 0, 
+        dateFormat: "dd/mm/yy",
+    });
+
+    function populateTable(tableId, data){
+        let table = $(tableId).get(0);
+        // clear table body
+        $(tableId).find("td").remove();
+
+        data.forEach((rowData) => {
+            let row = document.createElement("tr");
+            rowData.forEach((cellData) => {
+                let cell = document.createElement("td");
+                cell.textContent = cellData;
+                row.appendChild(cell);
+            });
+            table.appendChild(row);
+        });
+    }
+
+    populateTable("#overdue_table", overdue); 
+    populateTable("#imminent_table", imminent);
+    $("#date_picker").datepicker("setDate", deadline);
+
+    let progressChart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+            labels: ["In Progress", "Completed", "Overdue"],
+            datasets: [
+                {
+                    data: data,
+                    backgroundColor: ["#888", "#D9D9D9", "#FF7A00"],  
+                },
+            ],
+        },
+    });
+
+    projectDropdown.on
+
+    $("#project_dropdown").change(()=>{
+        let selectedProject = $("#project_dropdown").val();
+        selectedProject = selectedProject.split(" ").join("").toLowerCase();
+        data = projectData[selectedProject].data;
+        overdue = projectData[selectedProject].overdue;
+        imminent = projectData[selectedProject].imminent;
+        deadline = projectData[selectedProject].deadline;
+
+        populateTable("#overdue_table", overdue); 
+        populateTable("#imminent_table", imminent);
+        $("#date_picker").datepicker("setDate", deadline);
+
+        progressChart.data.datasets[0].data = data;
+        progressChart.update();
+    });
+    });
