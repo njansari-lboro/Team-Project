4c39cd02f386e34dd2e7f17a398d3ebf5c773e4f
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-10-26 14:13:31 +0100

    Fixed icon paths

--- pages/index.php
+++ pages/index.php
@@ -14,7 +14,7 @@
 
     <body>
         <div class="nav-bar">
-            <load-svg id="sidebar-toggle" src="assets/sidebarToggleIcon.svg">
+            <load-svg id="sidebar-toggle" src="../assets/sidebarToggleIcon.svg">
                 <style shadowRoot>
                     svg {
                         height: 2em;
@@ -27,7 +27,7 @@
             </load-svg>
 
             <div>
-                <load-svg id="title-logo" class="logo" src="assets/titleLogo.svg">
+                <load-svg id="title-logo" class="logo" src="../assets/titleLogo.svg">
                     <style shadowRoot>
                         svg {
                             height: 4em;
@@ -39,7 +39,7 @@
                     </style>
                 </load-svg>
 
-                <load-svg id="simple-logo" class="logo" src="assets/logo.svg">
+                <load-svg id="simple-logo" class="logo" src="../assets/logo.svg">
                     <style shadowRoot>
                         svg {
                             height: 4em;
@@ -60,7 +60,7 @@
 
                 <div id="profile-menu">
                     <button id="profile-menu-button">
-                        <load-svg id="profile-icon" src="assets/profileIcon.svg">
+                        <load-svg id="profile-icon" src="../assets/profileIcon.svg">
                             <style shadowRoot>
                                 svg {
                                     height: 3em;
@@ -72,7 +72,7 @@
                             </style>
                         </load-svg>
 
-                        <load-svg id="profile-menu-arrow" src="assets/menuArrow.svg">
+                        <load-svg id="profile-menu-arrow" src="../assets/menuArrow.svg">
                             <style shadowRoot>
                                 svg {
                                     height: 0.8em;
@@ -97,7 +97,7 @@
         <div id="sidebar">
             <div id="sidebar-links">
                 <a id="dashboard-sidebar-item" class="sidebar-item" href="?page=dashboard">
-                    <load-svg class="sidebar-item-icon" src="assets/dashboardSidebarItemIcon.svg">
+                    <load-svg class="sidebar-item-icon" src="../assets/dashboardSidebarItemIcon.svg">
                         <style shadowRoot>
                             svg {
                                 width: 2.4em;
@@ -113,7 +113,7 @@
                 </a>
 
                 <a id="tasks-sidebar-item" class="sidebar-item" href="?page=tasks">
-                    <load-svg class="sidebar-item-icon" src="assets/tasksSidebarItemIcon.svg">
+                    <load-svg class="sidebar-item-icon" src="../assets/tasksSidebarItemIcon.svg">
                         <style shadowRoot>
                             svg {
                                 height: 2.6em;
@@ -129,7 +129,7 @@
                 </a>
 
                 <a id="todo-sidebar-item" class="sidebar-item" href="?page=todo">
-                    <load-svg class="sidebar-item-icon" src="assets/todoSidebarItemIcon.svg">
+                    <load-svg class="sidebar-item-icon" src="../assets/todoSidebarItemIcon.svg">
                         <style shadowRoot>
                             svg {
                                 width: 2.2em;
@@ -144,7 +144,7 @@
                 </a>
 
                 <a id="tutorials-sidebar-item" class="sidebar-item" href="?page=tutorials">
-                    <load-svg class="sidebar-item-icon" src="assets/tutorialsSidebarItemIcon.svg">
+                    <load-svg class="sidebar-item-icon" src="../assets/tutorialsSidebarItemIcon.svg">
                         <style shadowRoot>
                             svg {
                                 width: 2.4em;
@@ -159,7 +159,7 @@
                 </a>
 
                 <a id="forums-sidebar-item" class="sidebar-item" href="?page=forums">
-                    <load-svg class="sidebar-item-icon" src="assets/forumsSidebarItemIcon.svg">
+                    <load-svg class="sidebar-item-icon" src="../assets/forumsSidebarItemIcon.svg">
                         <style shadowRoot>
                             svg {
                                 width: 2.4em;
@@ -180,7 +180,7 @@
                 <div class="divider horizontal"></div>
 
                 <button id="invite-button">
-                    <load-svg src="assets/inviteIcon.svg">
+                    <load-svg src="../assets/inviteIcon.svg">
                         <style shadowRoot>
                             svg {
                                 width: 3em;
