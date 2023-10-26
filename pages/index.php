316a142e1bbc6b9fc8583ef364b1c4981f3c6dbd
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-10-26 14:15:23 +0100

    Fixed global.css path

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="global.css">
+        <link rel="stylesheet" href="../global.css">
         <link rel="stylesheet" href="style.css">
 
         <title>Make-It-All</title>
