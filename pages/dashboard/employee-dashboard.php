3fe56ad14228fc7fd94fc102149241dd91be8f87
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-01 16:46:37 +0000

    Completed merging update employee dashboard

--- pages/dashboard/employee-dashboard.php
+++ pages/dashboard/employee-dashboard.php
@@ -5,117 +5,138 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/global.css">
         <link rel="stylesheet" href="/pages/dashboard/employee-dashboard.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/moment@2.29.4/moment.min.js"></script>
     </head>
 
     <body>
-        <p id="today-date">use js to get date</p>
+        <h2 class="px-5 pt-3 pb-1" id="today">
+            <script>
+                let today = moment().format("dddd, Do MMMM")
+                document.write(today)
+            </script>
+        </h2>
 
-        <div class="general">
-            <div class="taskpreview">
-                <p> Upcoming tasks</p>
-                <table>
-                    <tr>
-                        <td id="taskname">Design UI</td>
-                        <td>Project 1</td>
-                        <td>26/10/23</td>
-                    </tr>
-                    <tr>
-                        <td id="taskname">Work on SQL</td>
-                        <td>Project 2</td>
-                        <td>24/10/23</td>
-                    </tr>
-                    <tr>
-                        <td id="taskname">Work with Jean for front end </td>
-                        <td>Project 3</td>
-                        <td>30/10/23</td>
-                    </tr>
-                    <tr>
-                        <td id="taskname">Prepare presentation</td>
-                        <td>Project 1</td>
-                        <td>22/11/23</td>
-                    </tr>
-                    <tr>
-                        <td id="taskname">Show to manager final product</td>
-                        <td>Project 3</td>
-                        <td>6/12/23</td>
-                    </tr>
-                </table>
+        <div class="container-fluid pt-2 pb-2 border rounded" id="dashboard">
+            <div class="row px-2 border rounded" id="tasks-preview">
+                <h3 class="text-center pt-1">Your Upcoming Tasks</h3>
 
-            </div>
+                <table class="table" id="tasks-table">
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Due Date</th>
+                        </tr>
+                    </thead>
 
-            <div class="projectpreview">
-                <p>Your Projects</p>
-                <table>
-                    <tr>
-                        <td>Project 1</td>
-                        <td> 6/11/23</td>
-                    </tr>
-                    <tr>
-                        <td>Project 2</td>
-                        <td>21/11/23</td>
-                    </tr>
-                    <tr>
-                        <td>Project 3</td>
-                        <td>1/12/23</td>
-                    </tr>
-
+                    <tbody>
+                        <tr>
+                            <td>Complete Project Report</td>
+                            <td>06/11/2023</td>
+                        </tr>
+                        <tr>
+                            <td>UI design</td>
+                            <td>20/11/2023</td>
+                        </tr>
+                        <tr>
+                            <td>Work on SQL</td>
+                            <td>01/12/2023</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
 
-            <div class="forumspreview">
-                <p> Recent Forums</p>
-                <table>
-                    <tr>
-                        <td>How to make coffee for boss</td>
-                    </tr>
-                    <tr>
-                        <td>When to contact Alice</td>
-                    </tr>
-                    <tr>
-                        <td>Meeting Preperation</td>
-                    </tr>
-                </table>
+            <div class="row px-1 border rounded" id="project-forums-preview">
+                <div class="col-md-6">
+                    <div class="pt-1"></div>
+
+                    <div class="row px-2 border rounded" id="project-preview">
+                        <h4 class="text-center pt-1">Your projects</h4>
+
+                        <table class="table" id="project-table">
+                            <thead>
+                                <tr>
+                                    <th>Project Name</th>
+                                    <th>Due date</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td>Project 1</td>
+                                    <td>12/11/23</td>
+                                </tr>
+                                <tr>
+                                    <td>Project 2</td>
+                                    <td>25/11/23</td>
+                                </tr>
+                                <tr>
+                                    <td>Project 3</td>
+                                    <td>7/12/23</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="pt-1"></div>
+
+                    <div class="row px-2 border rounded" id="forums-preview">
+                        <h4 class="text-center pt-1">Recent Forums</h4>
+                        <table class="table" id="forums-table">
+                            <thead>
+                                <tr>
+                                    <th>Form Name</th>
+                                    <th>Created By</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td>Meeting Preparation</td>
+                                    <td>Alice</td>
+                                </tr>
+                                <tr>
+                                    <td>Meeting Preparation</td>
+                                    <td>Alice</td>
+                                </tr>
+                                <tr>
+                                    <td>Meeting Preparation</td>
+                                    <td>Alice</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="pt-1"></div>
+                </div>
             </div>
+
+            <div class="pt-1"></div>
         </div>
 
         <script>
-            const date = new Date()
-            month = date.getMonth()
-            number = date.getDate()
-            day = date.getDay()
+            $(document).ready(() => {
+                $("#tasks-preview").click(() => {
+                    window.location.href = "?page=tasks"
+                })
 
-            const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-            const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+                $("#project-preview").click(() => {
+                    //ask what page to lead to
+                    // window.location.href = "?page=tasks"
+                })
 
-            for (let x = 0; x <= 11; x++) {
-                if (month == x) {
-                    chosenMonth = months[x]
-                }
-            }
-
-            chosenDay = ""
-
-            for (let y = 0; y <= 6; y++) {
-                if (day == y) {
-                    chosenDay = days[y]
-                }
-            }
-
-            suffix = ""
-
-            switch (number) {
-            case 1: case 21: case 31:
-                suffix = "st"
-            case 2: case 22:
-                suffix = "nd"
-            case 3: case 23:
-                suffix = "rd"
-            default:
-                suffix = "th"
-            }
-
-            document.getElementById("today-date").innerHTML = `${chosenDay} ${number}${suffix} ${chosenMonth}`
+                $("#forums-preview").click(() => {
+                    window.location.href = "?page=forums"
+                })
+            })
         </script>
     </body>
 </html>
