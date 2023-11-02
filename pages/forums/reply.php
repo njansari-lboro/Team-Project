73854252a95f510a0d1605b3e43e7b25531f3de7
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-02 23:15:37 +0000

    Changes to reply php and css

--- pages/forums/reply.php
+++ pages/forums/reply.php
@@ -1,39 +1,37 @@
 <!DOCTYPE html>
 
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/pages/forums/reply.css">
 
-    <link rel="stylesheet" href="/pages/forums/reply.css">
+        <title>Question Modal</title>
+    </head>
 
-    <title>Question Modal</title>
-</head>
+    <body>
+        <div class="modal-container">
+            <div class="row flex-row">
+                <button class="exit-btn">×</button>
+            </div>
 
-<body>
-    <div class="modal-container">
-        <div class="row flex-row">
-            <button class="exit-btn">×</button>
+            <div class="row">
+                <textarea class="textarea" placeholder="Enter your comment"></textarea>
+            </div>
+
+            <div class="row">
+                <button class="post-btn">
+                    <span class="envelope-icon">✉️</span> Post
+                </button>
+            </div>
         </div>
+    </body>
 
-        <div class="row">
-            <textarea class="textarea" placeholder="Enter your comment"></textarea>
-        </div>
-
-        <div class="row">
-            <button class="post-btn">
-                <span class="envelope-icon">✉️</span> Post
-            </button>
-        </div>
-    </div>
-</body>
-
-<script>
-    let exitButton = document.querySelector(".exit-btn")
-    exitButton.addEventListener("click", () => {
-        window.location.href = "index.php?page=forums&task=view"
-    })
-</script>
-
+    <script>
+        let exitButton = document.querySelector(".exit-btn")
+        exitButton.addEventListener("click", () => {
+            window.location.href = "index.php?page=forums&task=view"
+        })
+    </script>
 </html>
