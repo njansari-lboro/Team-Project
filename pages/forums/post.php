f987e650aa8b44f3664d520916ceee29b055f46d
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-02 22:47:41 +0000

    Changes to new post php and css

--- pages/forums/post.php
+++ pages/forums/post.php
@@ -1,50 +1,48 @@
 <!DOCTYPE html>
 
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/pages/forums/post.css">
 
-    <link rel="stylesheet" href="/pages/forums/post.css">
+        <title>Question Modal</title>
+    </head>
 
-    <title>Question Modal</title>
-</head>
+    <body>
+        <!-- Modal/Overlay Container -->
+        <div class="modal-container">
+            <div class="row flex-row">
+                <button class="exit-btn">×</button>
 
-<body>
-    <!-- Modal/Overlay Container -->
-    <div class="modal-container">
-        <div class="row flex-row">
-            <button class="exit-btn">×</button>
+                <select class="dropdown" id="topicDropdown">
+                    <option value="default" disabled selected>Topic</option>
+                    <option value="Software Development">Software Development</option>
+                    <option value="Software Issues">Software Issues</option>
+                    <option value="Printing">Printing</option>
+                    <option value="newTopic">+ New Topic</option>
+                </select>
 
-            <select class="dropdown" id="topicDropdown">
-                <option value="default" disabled selected>Topic</option>
-                <option value="Software Development">Software Development</option>
-                <option value="Software Issues">Software Issues</option>
-                <option value="Printing">Printing</option>
-                <option value="newTopic">+ New Topic</option>
-            </select>
+                <input type="text" class="input" placeholder="Enter question title">
+            </div>
 
-            <input type="text" class="input" placeholder="Enter question title">
+            <div class="row">
+                <textarea class="textarea" placeholder="Enter question body"></textarea>
+            </div>
+
+            <div class="row">
+                <button class="post-btn">
+                    <span class="envelope-icon">✉️</span> Post
+                </button>
+            </div>
         </div>
+    </body>
 
-        <div class="row">
-            <textarea class="textarea" placeholder="Enter question body"></textarea>
-        </div>
-
-        <div class="row">
-            <button class="post-btn">
-                <span class="envelope-icon">✉️</span> Post
-            </button>
-        </div>
-    </div>
-</body>
-
-<script>
-    let exitButton = document.querySelector(".exit-btn")
-    exitButton.addEventListener("click", () => {
-        window.location.href = "index.php?page=forums"
-    })
-</script>
-
+    <script>
+        let exitButton = document.querySelector(".exit-btn")
+        exitButton.addEventListener("click", () => {
+            window.location.href = "index.php?page=forums"
+        })
+    </script>
 </html>
