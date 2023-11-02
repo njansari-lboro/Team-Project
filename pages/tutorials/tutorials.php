b04e75c1074265b0caa96647f3fee0abb1b3a6c4
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-02 12:43:17 +0000

    It works?

--- pages/tutorials/tutorials.php
+++ pages/tutorials/tutorials.php
@@ -45,7 +45,7 @@ function display_default()
                 <path d="M19 11H13V5C13 4.45 12.55 4 12 4C11.45 4 11 4.45 11 5V11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13H11V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19V13H19C19.55 13 20 12.55 20 12C20 11.45 19.55 11 19 11Z" />
             </svg>
         </a>
-        <?php include('/helpers/dynamic-carousel.php'); ?>
+        <?php include('../helpers/dynamic-carousel.php'); ?>
 
 
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
