22f3e987548b9682df61304d2cc86006a4d20593
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-02 12:39:05 +0000

    New debug msg

--- pages/tutorials/tutorials.php
+++ pages/tutorials/tutorials.php
@@ -36,7 +36,7 @@ function display_default()
                 <path d="M19 11H13V5C13 4.45 12.55 4 12 4C11.45 4 11 4.45 11 5V11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13H11V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19V13H19C19.55 13 20 12.55 20 12C20 11.45 19.55 11 19 11Z" />
             </svg>
         </a>
-        <?php echo getcwd(); include '/helpers/dynamic-carousel.php'; ?>
+        <?php echo get_include_path(); include '/helpers/dynamic-carousel.php'; ?>
         <br><br>
         <h1 class="tutHeader">Non Technical Information</h1>
         <!-- IF NOT TECHINICAL HIDE -->
