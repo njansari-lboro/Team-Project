9486287a0d5641f15b48e6e86af0b1a46e80a26e
Ben Hamblin <83784815+benhamblin@users.noreply.github.com>
2023-11-03 23:05:58 +0000

    wip

--- helpers/emailcheck.php
+++ helpers/emailcheck.php
@@ -1,34 +1,41 @@
 <?php
-    $emails = [
-        "admin@make-it-all.co.uk",
-        "dilip@make-it-all.co.uk",
-        "employee@make-it-all.co.uk",
-        "ben@make-it-all.co.uk",
-        "nayan@make-it-all.co.uk",
-        "iori@make-it-all.co.uk",
-        "aaron@make-it-all.co.uk",
-        "max@make-it-all.co.uk",
-        "shannon@make-it-all.co.uk",
-        "yuelin@make-it-all.co.uk"
-    ];
+$emails = [
+    "admin@make-it-all.co.uk",
+    "dilip@make-it-all.co.uk",
+    "employee@make-it-all.co.uk",
+    "ben@make-it-all.co.uk",
+    "nayan@make-it-all.co.uk",
+    "iori@make-it-all.co.uk",
+    "aaron@make-it-all.co.uk",
+    "max@make-it-all.co.uk",
+    "shannon@make-it-all.co.uk",
+    "yuelin@make-it-all.co.uk"
+];
 
-    session_start();
-    // include("db_conn.php");  
+session_start();
+// include("db_conn.php");  
 
-    $useremail = $_POST["email"];
+$useremail = isset($_POST["email"]) ? $_POST["email"] : null;
+$inviteCode = isset($_POST["invite_code"]) ? $_POST["invite_code"] : null;
 
-    $isValidUser = false;
+$isValidUser = false;
 
+if ($inviteCode) {
+
+    if (in_array($inviteCode, $emails)) {
+        $isValidUser = true;
+    }
+} else {
     foreach ($emails as $email) {
         if ($useremail == $email) {
             $isValidUser = true;
             break;
         }
     }
-    
-    if ($isValidUser) {
-        echo "true";
-    } else {
-        echo "false";
-    }
-?>
+}
+
+if ($isValidUser) {
+    echo "true";
+} else {
+    echo "false";
+}
