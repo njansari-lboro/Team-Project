bb032e577bd4d0db04fb8d5051d7ccdabaf86e35
Ben Hamblin <83784815+benhamblin@users.noreply.github.com>
2023-11-03 17:19:27 +0000

    test draggab

--- pages/projects/accordian-projects.php
+++ pages/projects/accordian-projects.php
@@ -8,13 +8,10 @@
 
         <!-- Add Bootstrap CSS and JS -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="jquery-ui-1.13.2.progress/jquery-ui.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://code.jquery.com/jquery-ui-1.13.2.date2/jquery-ui.css">
         <link rel="stylesheet" type="text/css" href="/pages/projects/project-drag.css">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+        <link rel="stylesheet" type="text/css" href="/global.css">
         <script>
             $(function () {
                 $("#accordion").accordion({
@@ -347,5 +344,9 @@
             </div>
         </div>
     </body>
-    <script src="/pages/projects/project-drag.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 </html>
