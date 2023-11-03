fe524f988d3d82f31a503565d8c4beabe13e49a7
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-03 22:55:50 +0000

    Fixed the add post button

--- pages/forums/forums.php
+++ pages/forums/forums.php
@@ -49,16 +49,12 @@
                     <load-svg src="/assets/addIcon.svg">
                         <style shadowRoot>
                             svg {
-                                width: 50px;
-                                height: 50px;
+                                width: 40px;
+                                height: 40px;
                             }
 
-                            :hover .fill {
-                                fill: var(--accent-color);
-                            }
-
-                            :hover .fill {
-                                fill: var(--selected-content-text-color)
+                            .fill {
+                                fill: var(--icon-color);
                             }
                         </style>
                     </load-svg>
