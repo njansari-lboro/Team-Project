db2422cf35a2f49a97b00ae0c5c3a8ca546d1bda
Ben Hamblin <83784815+benhamblin@users.noreply.github.com>
2023-11-03 17:32:30 +0000

    touch monkey test

--- pages/projects/add-project-tasks.php
+++ pages/projects/add-project-tasks.php
@@ -138,6 +138,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script src="jquery.ui.touch-punch.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="/pages/projects/add-project-tasks.js"></script>
 </html>
