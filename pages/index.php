50b9c56ede806ed1ca67b37dac7d2726943f5da2
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-03 23:03:45 +0000

    Changed forums sidebar item icon

--- pages/index.php
+++ pages/index.php
@@ -232,6 +232,7 @@
                         <style shadowRoot>
                             svg {
                                 width: 2.4em;
+                                padding-bottom: 0.1em
                             }
 
                             .fill {
