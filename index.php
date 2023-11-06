c58b4fd09ed4cea8ddc97dd1c3ad6d6fe64e86ba
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-06 13:05:48 +0000

    Added ycsm logo

--- index.php
+++ index.php
@@ -155,6 +155,10 @@
             </div>
         </div>
 
+        <div style="position: fixed; right: 10; bottom: 10">
+            <img src="/img/ycsmLogo.png" width="50px">
+        </div>
+
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         <script type="text/javascript" src="framework.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
