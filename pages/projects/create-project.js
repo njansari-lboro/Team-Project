26210f6457f6fb7c4559129d43d6299f4d698e3e
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-06 11:53:38 +0000

    Removed debug msg

--- pages/projects/create-project.js
+++ pages/projects/create-project.js
@@ -2,7 +2,6 @@ $(() => {
     const urlParams = new URLSearchParams(window.location.search);
 
     if (urlParams.has("project_name")) {
-        console.log("has param")
         const projectName = urlParams.get("project_name");
         $("#pname").val(projectName);
     }
