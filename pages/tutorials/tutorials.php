0a27c5dbb318356607951780c83b46b351177d73
Ben Hamblin <83784815+benhamblin@users.noreply.github.com>
2023-11-07 14:10:55 +0000

    testing permissions

--- pages/tutorials/tutorials.php
+++ pages/tutorials/tutorials.php
@@ -38,12 +38,14 @@
 
     <body class="customBody">
         <h1 class="tutHeader">Technical Information</h1>
-
-        <a href="?page=tutorials&task=new_tut&technical=1" class="plus-icon-link">
+        <?php if ($_SESSION['user']['role'] != 'Employee'){
+        echo'<a href="?page=tutorials&task=new_tut&technical=1" class="plus-icon-link">
             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M19 11H13V5C13 4.45 12.55 4 12 4C11.45 4 11 4.45 11 5V11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13H11V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19V13H19C19.55 13 20 12.55 20 12C20 11.45 19.55 11 19 11Z" />
             </svg>
-        </a>
+        </a>';
+}
+    ?>
 
         <?php include "tutorials/dynamic-carousel.php" ?>
 
