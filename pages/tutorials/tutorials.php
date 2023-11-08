5a712ba84e858198223adea854ea52ee906001dc
Nayan <114915046+njansari-lboro@users.noreply.github.com>
2023-11-08 00:04:19 +0000

    Fixed tutorial heading margins

--- pages/tutorials/tutorials.php
+++ pages/tutorials/tutorials.php
@@ -37,28 +37,31 @@
     </head>
 
     <body class="customBody">
-        <h1 class="tutHeader">Technical Information</h1>
+        <div class="tutHeaderSection">
+            <h1 class="tutHeader">Technical Information</h1>
 
-        <?php if ($_SESSION["user"]["role"] != "Employee") { ?>
-        <a href="?page=tutorials&task=new_tut&technical=1" class="plus-icon-link">
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M19 11H13V5C13 4.45 12.55 4 12 4C11.45 4 11 4.45 11 5V11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13H11V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19V13H19C19.55 13 20 12.55 20 12C20 11.45 19.55 11 19 11Z" />
-            </svg>
-        </a>
-        <?php } ?>
+            <?php if ($_SESSION["user"]["role"] != "Employee") { ?>
+            <a href="?page=tutorials&task=new_tut&technical=1" class="plus-icon-link">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 11H13V5C13 4.45 12.55 4 12 4C11.45 4 11 4.45 11 5V11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13H11V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19V13H19C19.55 13 20 12.55 20 12C20 11.45 19.55 11 19 11Z" />
+                </svg>
+            </a>
+            <?php } ?>
+        </div>
 
         <?php include "tutorials/dynamic-carousel.php" ?>
 
         <br><br>
 
-        <h1 class="tutHeader">Non Technical Information</h1>
-
-        <!-- IF NOT TECHINICAL HIDE -->
-        <a href="?page=tutorials&task=new_tut&technical=0" class="plus-icon-link">
-            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M19 11H13V5C13 4.45 12.55 4 12 4C11.45 4 11 4.45 11 5V11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13H11V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19V13H19C19.55 13 20 12.55 20 12C20 11.45 19.55 11 19 11Z" />
-            </svg>
-        </a>
+        <div class="tutHeaderSection">
+            <h1 class="tutHeader">Non Technical Information</h1>
+            
+            <a href="?page=tutorials&task=new_tut&technical=0" class="plus-icon-link">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 11H13V5C13 4.45 12.55 4 12 4C11.45 4 11 4.45 11 5V11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13H11V19C11 19.55 11.45 20 12 20C12.55 20 13 19.55 13 19V13H19C19.55 13 20 12.55 20 12C20 11.45 19.55 11 19 11Z" />
+                </svg>
+            </a>
+        </div>
 
         <?php include("tutorials/dynamic-carousel.php") ?>
 
